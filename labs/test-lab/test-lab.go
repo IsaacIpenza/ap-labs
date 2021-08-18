58d8d414b344476423c31f48e248f5ada3acdfe2
IsaacIpenza <benjamin.ipre@gmail.com>
2021-08-18 00:21:27 +0000

    Solve test-lab

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+	
+	if len(os.Args) == 2 {
+		name := os.Args[1]
+		fmt.Printf("Welcome %v to the jungle \n", name)
+	} else if len(os.Args) > 2{
+		allArgs := os.Args[1:]
+		
+		fmt.Printf("Welcome ")
+		
+		for _,str := range allArgs{
+			fmt.Printf("%s ", str)
+		}
+
+		fmt.Printf("to the jungle \n")
+
+	} else{
+		fmt.Println("No argument !!!")
+	}
+	
 }
